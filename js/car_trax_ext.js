ea7250e503ca67822415a25f5d85a573f0aca001
wushuyi <wushuyi541@gmail.com>
2014-08-25 17:19:20 +0800

    bug修护

--- js/car_trax_ext.js
+++ js/car_trax_ext.js
@@ -2,10 +2,6 @@ $(document).ready(function(){
 	var thisVideo_1 = document.getElementById("video_1");
 	var thisVideo_2 = document.getElementById("video_2");
 	var thisVideo_3 = document.getElementById("video_3");
-	var thisVideo_4 = document.getElementById("video_4");
-	var thisVideo_5 = document.getElementById("video_5");
-	var thisVideo_6 = document.getElementById("video_6");
-	var thisVideo_7 = document.getElementById("video_7");
 
 	if(thisVideo_1){
 		thisVideo_1.addEventListener('pause',function(){
@@ -17,20 +13,7 @@ $(document).ready(function(){
 		thisVideo_3.addEventListener('pause',function(){
 			$(".hide_video_3").hide();
 		})
-		thisVideo_4.addEventListener('pause',function(){
-			$(".hide_video_4").hide();
-		})
-		thisVideo_5.addEventListener('pause',function(){
-			$(".hide_video_5").hide();
-		})
-		thisVideo_6.addEventListener('pause',function(){
-			$(".hide_video_6").hide();
-		})
-		thisVideo_7.addEventListener('pause',function(){
-			$(".hide_video_7").hide();
-		})
 	}
-
 
 	$(".car_detail .video_1").click(function(){
 		$(".hide_video_1").show();
@@ -45,26 +28,6 @@ $(document).ready(function(){
 	$(".car_detail .video_3").click(function(){
 		$(".hide_video_3").show();
 		thisVideo_3.play();
-		return false;
-	});
-	$(".car_detail .video_4").click(function(){
-		$(".hide_video_4").show();
-		thisVideo_4.play();
-		return false;
-	});
-	$(".car_detail .video_5").click(function(){
-		$(".hide_video_5").show();
-		thisVideo_5.play();
-		return false;
-	});
-	$(".car_detail .video_6").click(function(){
-		$(".hide_video_6").show();
-		thisVideo_6.play();
-		return false;
-	});
-	$(".car_detail .video_7").click(function(){
-		$(".hide_video_7").show();
-		thisVideo_7.play();
 		return false;
 	});
 });
